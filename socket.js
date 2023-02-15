a916d320b7a2019541f5cb7fad163432291a481c
Taufiq Ridwan <taufiq26.ridwan@gmail.com>
2023-02-15 16:39:49 +0700

    Update socket.js

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res){
-	fs.readFile('./index_socket.html', 'utf-8', function(error, content){
+	fs.readFile('./index.html', 'utf-8', function(error, content){
 		res. writeHead(200, {"Content-Type": "text/html"});
 		res.end(content);
 	});
